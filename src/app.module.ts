db359e8dd276b9c817b1f2a2e1f6de43f90567df
Wardi <wardi@nusa.net.id>
2022-08-12 16:33:42 +0700

    add hello module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { HelloModule } from './hello/hello.module';
 
 @Module({
-  imports: [],
+  imports: [HelloModule],
   controllers: [],
   providers: [],
 })
