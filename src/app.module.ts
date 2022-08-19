0b5bbd264518254728fa01abc5f01e9a73e8b417
Wardi <wardi@nusa.net.id>
2022-08-19 08:54:12 +0700

    add TypeOrm database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 import { HelloModule } from './hello/hello.module';
 import { AuthModule } from './auth/auth.module';
@@ -7,6 +9,15 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'mysql' | 'mariadb' | 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DBNAME,
+      entities: [],
+      synchronize: true}),
     HelloModule,
     AuthModule
   ],
