72c273e9b8403ef8f8083060019f7627650889ff
SamuelNusanetID <samuel@nusa.net.id>
2022-12-07 10:51:24 +0700

    Fix: Perbaikan Routes URL untuk Meta Pagination Fitur Promo

--- src/sales-promo/sales-promo.controller.ts
+++ src/sales-promo/sales-promo.controller.ts
@@ -5,32 +5,37 @@ import {
   HttpStatus,
   ParseIntPipe,
   Query,
-  UseGuards,
+  Req,
+  // UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { GetPromoFilterDto } from './dto/get-promo-filter.dto';
 import { SalesPromoService } from './sales-promo.service';
-import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+// import { AuthGuard } from '@nestjs/passport';
 
-@UseGuards(AuthGuard('api-key'))
+// @UseGuards(AuthGuard('api-key'))
 @Controller('promo')
 export class SalesPromoController {
   constructor(private salesPromoService: SalesPromoService) {}
 
   @Get()
   async getAllDataPromo(
+    @Req() req: Request,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
     @Query(ValidationPipe) filterPromoDto: GetPromoFilterDto,
   ) {
     try {
+      const url_api = `${req.protocol}://${req.get('Host')}${req.originalUrl}`;
+
       limit = limit > 100 ? 100 : limit;
       const resultAllPromo = await this.salesPromoService.getAllPromoService(
         filterPromoDto,
         {
           page,
           limit,
-          route: 'http://127.0.0.1:3000/api/v1/promo/testpage',
+          route: url_api,
         },
       );
 
@@ -43,7 +48,7 @@ export class SalesPromoController {
       return {
         statusCode: HttpStatus.BAD_GATEWAY,
         message: {},
-        error: error,
+        error: error.message,
       };
     }
   }
